5f0152f7dd15efb4ed18a811929e7c44ff030686
rajumms1998 <rajumms1998@gmail.com>
2023-12-26 23:09:59 +0530

    prodwebsite changes

--- cypress/e2e/Stage/DashPom.cy.js
+++ cypress/e2e/Stage/DashPom.cy.js
@@ -1,16 +1,18 @@
 
 export class LoginPage {
 
-
+    prodnumber;
     number;
     constructor()
          {
     this.number = "9876543211";
+    this.prodnumber="8309333476";
     }
     
     loginnnnnn(){
     
-    cy.visit("http://43.205.92.177",{failOnStatusCode:false})
+    cy.visit("http://v2.nushop-dashboard.kaip.in",{failOnStatusCode:false})
+    
     cy.wait(2000)
     cy.get('.rs-input').type(this.number)
     cy.get('.Button_button-primary__9i0Rz').click()
@@ -19,7 +21,24 @@ export class LoginPage {
     
    }
 
+prodlogin(){
 
+     cy.visit("https://pro.shopdeck.com",{failOnStatusCode:false})
+    
+     cy.wait(2000)
+     cy.get('.rs-input').type(this.prodnumber)
+     cy.get('.Button_button-primary__9i0Rz').click()
+
+    // cy.xpath('//*[@id="root"]/main/div/div/div/div[2]/div[1]/input').click().type("0000")
+    cy.wait (12000)
+     cy.get('.Button_button-primary__9i0Rz').click()
+
+
+
+
+
+
+}
 
    }
 
