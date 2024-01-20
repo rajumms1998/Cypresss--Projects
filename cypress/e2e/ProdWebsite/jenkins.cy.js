dc844edbb2bf2db79f72d0ffd191606f67c437c2
rajumms1998 <rajumms1998@gmail.com>
2024-01-20 19:51:20 +0530

    changes on 20th jan

---
+++ cypress/e2e/ProdWebsite/jenkins.cy.js
@@ -0,0 +1,17 @@
+describe("Recently Viewed Activity", function() {
+    it("Viewing the Product and Checking in the Recently Viewed Widget", function() {
+
+let x=10;
+let y=20;
+cy.log(x+y);
+
+
+
+
+
+
+
+
+
+    })
+})
